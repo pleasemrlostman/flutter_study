bf8c4fe7db95c65860c860c570779dfca5a54b90
jh <pleasemrlostman@naver.com>
2024-05-28 15:20:14 +0900

    section 7 slasph screen end

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,30 @@ void main() {
   );
 }
 
-// StatelessWidget
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // 335cb0
       backgroundColor: Color(0xFF335CB0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "asset/img/logo.png",
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          ),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "asset/img/logo.png",
+            ),
+            SizedBox(
+              height: 28.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
