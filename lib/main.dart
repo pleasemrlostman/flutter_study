e40c90fd8f97e71884210e3ffefea5f8602e9fae
jh <pleasemrlostman@naver.com>
2024-05-29 10:30:43 +0900

    section9 프로젝트세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:test_prog/screen/home_screen.dart';
+import 'package:test_prog/screen/home_screen_padding.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      home: HomeScreenPadding(),
+    ),
   );
 }
---
+++ lib/screen/home_screen_padding.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+import 'package:test_prog/const/colors.dart';
+
+class HomeScreenPadding extends StatelessWidget {
+  const HomeScreenPadding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.red,
+          child: Container(
+            color: Colors.blue,
+            width: 50.0,
+            height: 50.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
