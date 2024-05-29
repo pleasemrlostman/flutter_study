cd75d3b092e729e0aad5df4a632205bb4e9753a8
jh <pleasemrlostman@naver.com>
2024-05-29 16:52:41 +0900

    s12 statefulWidget LIfe Cycle 1/2

--- lib/screen/life_cycle/statefull/home_screen.dart
+++ lib/screen/life_cycle/statefull/home_screen.dart
@@ -38,15 +38,59 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class CodeFactoryWidget extends StatelessWidget {
-  const CodeFactoryWidget({super.key});
+class CodeFactoryWidget extends StatefulWidget {
+  CodeFactoryWidget({super.key}) {
+    print("1) Stateful Widget Constructor");
+  }
+
+  @override
+  State<CodeFactoryWidget> createState() {
+    print("2) Stateful Widget Create State");
+    return _CodeFactoryWidgetState();
+  }
+}
+
+class _CodeFactoryWidgetState extends State<CodeFactoryWidget> {
+  Color color = Colors.red;
+
+  @override
+  void initState() {
+    print("3) Stateful Widget initState");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("4) Stateful Widget didChangeDependencies");
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      width: 50.0,
-      height: 50.0,
+    print("5) Stateful Widget build");
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          color = color == Colors.red ? Colors.blue : Colors.red;
+        });
+      },
+      child: Container(
+        color: color,
+        width: 50.0,
+        height: 50.0,
+      ),
     );
+  }
+
+  @override
+  void deactivate() {
+    print("6) Stateful Widget deactivate");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print("7) Stateful Widget dispose");
+    super.dispose();
   }
 }
