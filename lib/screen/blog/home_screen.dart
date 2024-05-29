ec6823b7853c64b3553b4f7686de01063ba7706d
jh <pleasemrlostman@naver.com>
2024-05-29 11:54:39 +0900

    s10 웹사이트 웹뷰에 띄우기 & 다큐멘테이션 읽어보기

--- lib/screen/blog/home_screen.dart
+++ lib/screen/blog/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  WebViewController controller = WebViewController()..loadRequest(homeUrl);
+  // 점을 두 개를 넣으면
+  // 이 함수를 실행하는데
+  // 함수를 실행한 결과 값을 반환하는게 아니라
+  // 우리가 함수를 실행한 대상을 반환해라
+  // final controller2 = WEB
 
   HomeScreen({super.key});
 
