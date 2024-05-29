70899ee4f88b6ce32a2d49781a989f1abb8b4155
jh <pleasemrlostman@naver.com>
2024-05-29 15:14:52 +0900

    s12 stateless widget 라이프 사이클 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test_prog/screen/stateful/home_screen.dart';
+// import 'package:test_prog/screen/stateful/home_screen.dart';
+import 'package:test_prog/screen/life_cycle/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
