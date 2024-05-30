fbd252070e3fe05357a977bcaa1d042c222f2a5b
jh <pleasemrlostman@naver.com>
2024-05-30 16:59:01 +0900

    s18 프로젝트 기본세팅

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -6,3 +6,7 @@ const colors = [
   Colors.yellow,
   Colors.green,
 ];
+
+const Color primaryColor = Color(0xFF2C2C63);
+const Color redColor = Color(0xFFEA4955);
+const Color blueColor = Color(0xFF549FBF);
