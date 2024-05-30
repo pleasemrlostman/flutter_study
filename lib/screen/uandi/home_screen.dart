4c51d860d8046890fb3feb452a9bcf56be9d2cad
jh <pleasemrlostman@naver.com>
2024-05-30 15:25:01 +0900

    s15 테마 적용해보기

--- lib/screen/uandi/home_screen.dart
+++ lib/screen/uandi/home_screen.dart
@@ -5,7 +5,71 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              // 텍스트
+              _Top(),
+              // 이미지
+              _Bottom(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
 
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'U&I',
+              style: textTheme.displayLarge,
+            ),
+            Text(
+              '우리 처음 만난날',
+              style: textTheme.bodyLarge,
+            ),
+            Text("2023-11-23", style: textTheme.bodyMedium),
+            IconButton(
+              iconSize: 60.0,
+              color: Colors.red,
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+              ),
+            ),
+            Text('D-1', style: textTheme.bodyLarge)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset('asset/img/middle_image.png'),
+      ),
+    );
+  }
 }
