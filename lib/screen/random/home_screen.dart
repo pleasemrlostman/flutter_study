6b609bc0366a96e077c0f02200a4cacff6028139
jh <pleasemrlostman@naver.com>
2024-05-31 10:22:41 +0900

    s18 ui 디자인하고 코드 정리하기

--- lib/screen/random/home_screen.dart
+++ lib/screen/random/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_prog/const/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,38 +7,84 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            //   제목과 아이콘 버튼이 있는곳
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("제목"),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                  ),
-                ),
-              ],
-            ),
-            //   숫자가 있는곳
-            Expanded(
-              child: Text('''
-              123
-              456
-              789
-              '''),
-            ),
-            //   버튼이 있는곳
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("생성하기-!"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //   제목과 아이콘 버튼이 있는곳
+              _Header(),
+              //   숫자가 있는곳
+              _Body(),
+              //   버튼이 있는곳
+              _Footer(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "랜덤숫자 생성기",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          color: redColor,
+          onPressed: () {},
+          icon: Icon(
+            Icons.settings,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text('''
+                123
+                456
+                789
+                '''),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      child: Text("생성하기-!"),
     );
   }
 }
