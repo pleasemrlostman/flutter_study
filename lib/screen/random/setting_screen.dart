3110c6f0e7e965feb4ca723d31209dd0d525ad0c
jh <pleasemrlostman@naver.com>
2024-05-31 15:12:28 +0900

    s18 세팅화면에 숫자 렌더링하기

--- lib/screen/random/setting_screen.dart
+++ lib/screen/random/setting_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:test_prog/const/colors.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  double maxNumber = 1000;
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,67 @@ class SettingScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: redColor,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("저장"),
-              )
+              _Button(),
+              _Number(maxNumber: maxNumber),
+              _Slider(),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      child: Text("저장"),
+    );
+  }
+}
+
+class _Number extends StatelessWidget {
+  double maxNumber;
+  _Number({super.key, required this.maxNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Row(
+          children: maxNumber
+              .toInt()
+              .toString()
+              .split("")
+              .map(
+                (number) => Image.asset(
+                  'asset/img/$number.png',
+                  width: 50.0,
+                  height: 70.0,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _Slider extends StatelessWidget {
+  const _Slider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
