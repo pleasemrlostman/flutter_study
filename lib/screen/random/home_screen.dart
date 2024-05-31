087ae319520a04ef1150ad977a89966d48cb0a0b
jh <pleasemrlostman@naver.com>
2024-05-31 10:13:40 +0900

    s18 레이아웃 작업하기

--- lib/screen/random/home_screen.dart
+++ lib/screen/random/home_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
-class  extends StatelessWidget {
-  const ({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            //   제목과 아이콘 버튼이 있는곳
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("제목"),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.settings,
+                  ),
+                ),
+              ],
+            ),
+            //   숫자가 있는곳
+            Expanded(
+              child: Text('''
+              123
+              456
+              789
+              '''),
+            ),
+            //   버튼이 있는곳
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("생성하기-!"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
